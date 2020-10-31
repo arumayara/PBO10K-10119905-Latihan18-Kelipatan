fa05e0511b0b7ca85c7d7acb845e9cc1692d7c58
Abraham Rumayara <72974847+arumayara@users.noreply.github.com>
2020-10-31 16:50:11 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan18-Kelipatan/src/main/java/PBO10K/NIM10119905/Latihan18/Kelipatan/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan18.Kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program Kelipatan
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i=1 ; i<=10 ; i++){
+            System.out.println(i*3.5);
+            }
+    }
+    
+}
+                         
+
